5bb8541bb076b545c7898037c1ff44a94138a0ef
bahram <bahram101@mail.ru>
2022-09-09 19:34:12 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import express from "express";
+import jwt from 'jsonwebtoken';
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+    res.send("Hello world");
 });
+
+app.post("/login", (req, res) => {
+    console.log(req.body)
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Бахрам Атаханов'
+    }, 'secret123')
+
+    res.json({ 
+        login: true,
+        token 
+    }); 
+});
+
+
+
+
+
 
 app.listen(4444, (err) => {
-    if(err){
-        return console.log('err')
+    if (err) {
+        return console.log("err");
     }
-    console.log('Server OK')
+    console.log("Server OK");
 });
-
-
