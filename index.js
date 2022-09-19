ef3922e61756aea9410b4dc2fcceed13d6b10576
bahram <bahram101@mail.ru>
2022-09-19 20:22:45 +0600

    upload images

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import multer from "multer";
+import cors from "cors";
 import { UserController, PostController } from "./controllers/index.js";
 import {
     registerValidation,
     loginValidation,
     postCreateValidation,
 } from "./validations.js";
-
 import { checkAuth, handleValidationErrors } from "./utils/index.js";
+
+const app = express();
 
 mongoose
     .connect(
@@ -17,9 +19,21 @@ mongoose
     .then(() => console.log("DB ok"))
     .catch((error) => console.log("DB error", error));
 
-const app = express();
+const storage = multer.diskStorage({
+    destination: (_, __, callBack) => {
+        callBack(null, "uploads");
+    },
+    filename: (_, file, callBack) => {
+        callBack(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
+
+app.use("/uploads", express.static("uploads"));
 
 app.post(
     "/auth/register",
@@ -34,6 +48,12 @@ app.post(
     UserController.login
 );
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
